b1517a728a3a1be0c2c4dd6e3f6641b48a89ff56
mallikarjun <mallikarjun.vksalgar@gmail.com>
2017-12-27 13:16:08 +0530

    new file is created.

--- src/com/avn/first/First.java
+++ src/com/avn/first/First.java
@@ -1,7 +1,10 @@
 package com.avn.first;
 
+import com.avn.second.Second;
+
 public class First {
 	public static void main(String[] args) {
 		System.out.println("First1");
+		Second s=new Second();
 	}
 }
---
+++ src/com/avn/second/Second.java
@@ -0,0 +1,8 @@
+package com.avn.second;
+
+public class Second {
+
+	public void display(){
+		System.out.println("second");
+	}
+}
