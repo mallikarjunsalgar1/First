ddff7db5c1c79486dbba540aa008a10b2b30841c
mallikarjun <mallikarjun.vksalgar@gmail.com>
2017-12-27 13:34:43 +0530

    compare with head revision

--- src/com/avn/first/First.java
+++ src/com/avn/first/First.java
@@ -6,6 +6,6 @@ public class First {
 	public static void main(String[] args) {
 		System.out.println("First1");
 		Second s=new Second();
-		System.out.println("third");
+		System.out.println("third1");
 	}
 }
