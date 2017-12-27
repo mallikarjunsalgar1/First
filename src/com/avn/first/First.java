f27c19c2e552d240cacffa21984a8bac7797667f
mallikarjun <mallikarjun.vksalgar@gmail.com>
2017-12-27 15:47:15 +0530

    branch commit

--- src/com/avn/first/First.java
+++ src/com/avn/first/First.java
@@ -4,8 +4,9 @@ import com.avn.second.Second;
 
 public class First {
 	public static void main(String[] args) {
-		System.out.println("First1");
+		System.out.println("First123");
 		Second s=new Second();
-		System.out.println("third1");
+		s.display();
+		System.out.println("third123");
 	}
 }
