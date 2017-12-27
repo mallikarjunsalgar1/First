d11040c8d6ca870d3f8df0da892332c43f0296d5
mallikarjun <mallikarjun.vksalgar@gmail.com>
2017-12-27 18:41:43 +0530

    testing

--- src/com/avn/first/First.java
+++ src/com/avn/first/First.java
@@ -4,9 +4,9 @@ import com.avn.second.Second;
 
 public class First {
 	public static void main(String[] args) {
-		System.out.println("First-branch merged chnages again testing ");
+		System.out.println("First-branch merged chnages again testing 1234 ");
 		Second s=new Second();
 		s.display();
-		System.out.println("third-branch merged changes1 agin testing ");
+		System.out.println("third-branch merged changes1 agin testing 1234");
 	}
 }
--- src/com/avn/second/Second.java
+++ src/com/avn/second/Second.java
@@ -3,7 +3,7 @@ package com.avn.second;
 public class Second {
 
 	public void display(){
-		System.out.println("second brach merged again testing 123");
-		System.out.println("Second1.1 brach merged again testing 123");
+		System.out.println("second brach merged again testing 1234");
+		System.out.println("Second1.1 brach merged again testing 1234");
 	}
 }
