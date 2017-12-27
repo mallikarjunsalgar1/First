8adea16ca26c3b2e3c8aa6aefcd3e5014b0a3756
mallikarjun <mallikarjun.vksalgar@gmail.com>
2017-12-27 17:49:19 +0530

    final commit

--- src/com/avn/first/First.java
+++ src/com/avn/first/First.java
@@ -4,9 +4,9 @@ import com.avn.second.Second;
 
 public class First {
 	public static void main(String[] args) {
-		System.out.println("First123");
+		System.out.println("First-branch merged");
 		Second s=new Second();
 		s.display();
-		System.out.println("third123");
+		System.out.println("third-branch merged");
 	}
 }
--- src/com/avn/second/Second.java
+++ src/com/avn/second/Second.java
@@ -3,7 +3,7 @@ package com.avn.second;
 public class Second {
 
 	public void display(){
-		System.out.println("second");
-		System.out.println("Second1.1");
+		System.out.println("second brach merged");
+		System.out.println("Second1.1 brach merged");
 	}
 }
