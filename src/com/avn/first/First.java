d3196f57a640e9311dcbbbaa33c9e59ea2350440
mallikarjun <mallikarjun.vksalgar@gmail.com>
2017-12-27 18:05:34 +0530

    branch changes

--- src/com/avn/first/First.java
+++ src/com/avn/first/First.java
@@ -4,9 +4,9 @@ import com.avn.second.Second;
 
 public class First {
 	public static void main(String[] args) {
-		System.out.println("First-branch merged");
+		System.out.println("First-branch merged chnages ");
 		Second s=new Second();
 		s.display();
-		System.out.println("third-branch merged");
+		System.out.println("third-branch merged changes ");
 	}
 }
